a4b798d6ddd12f689919ce64380359ca83c746f5
Ashley Frank <ashley.frank@ufl.edu>
2024-06-09 01:52:51 -0400

    Failed attempt to fix Iron Session

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "../styles/Home.module.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
+// import { withIronSessionSsr } from 'iron-session/next.js'
+// import sessionOptions from './config/session'
 
+// export const getServerSideProps = withIronSessionSsr (
+//   async function getServerSideProps({ req }) {
+//     const user = req.session.user;
+//     const props = {};
+//     if (user) {
+//       props.user = req.session.user;
+//       props.isLoggedIn = true;
+//     } else {
+//       props.isLoggedIn = false;
+//     }
 
+//     return { props };
+//   },
+//   sessionOptions
+// );
 
 export default function Home(props) {
   const router = useRouter();
