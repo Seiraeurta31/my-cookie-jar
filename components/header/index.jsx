cbd92655cac5d25175db6fa5d278fe11b80eaa66
Ashley Frank <ashley.frank@ufl.edu>
2024-07-28 22:15:41 -0400

    Header Bar styled

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -17,58 +17,61 @@ export default function Header(props) {
 
               <div className={styles.headerContainer}>
 
-                <div className={styles.logoandTitle}>
+                <div>
                     <Image src="/cookieJarLogo_Circle.png" className={styles.logoIcon} width="3000" height="1928"/>
-                    <div>
-                      <h1 className={styles.pageTitle}>{props.pageTitle}</h1>
-                    </div>
                 </div> 
 
-
-                {props.menu === "user" ? (
-                <>
-                    <div className={styles.navContainer}>
-                      <div >
-                        <Link href="/dashboard" className={styles.navLink}>Dashboard </Link>
-                      </div>
-                      <div>
-                        <Link href="/createGroup" className={styles.navLink}>Create Group    </Link> 
-                      </div>
-                      <div>
-                        <Link href="/joinGroup" className={styles.navLink}>Join Group    </Link>
-                      </div>
-                      <div>
-                        <a onClick={logout} style={{ cursor: "pointer" }} className={styles.navLink}>Logout</a> 
-                      </div>
-                    </div>  
-
-                  </>
-                  ) : (
-                    <div> 
-                    </div>
-                )}
-
-                {props.menu === "group" ? (
-                  <>
-                    <div className={styles.navContainer}>
-                      <div>
-                        <Link href="/dashboard" className={styles.navLink}>Dashboard </Link>
-                      </div>
-                      <div>
-                        <Link href={'/group/' + props.groupId} className={styles.navLink}>Home Page   </Link> 
-                      </div>
-                      <div>
-                        <Link href={`/membersList/${props.groupId}`} className={styles.navLink}>Members List    </Link>
-                      </div>
-                      <div>
-                        <a onClick={logout} style={{ cursor: "pointer" }} className={styles.navLink}>Logout</a> 
-                      </div>
-                    </div> 
-                  </>
-                ) : (
-                  <div>  
+                <div className={styles.titleAndNav}>
+                  <div>
+                    <h1 className={styles.pageTitle}>{props.pageTitle}</h1>
                   </div>
-                )}
+                  {props.menu === "user" ? (
+                    <>
+                      <div className={styles.navContainer}>
+                        <div >
+                          <Link href="/dashboard" className={styles.navLink}>Dashboard </Link>
+                        </div>
+                        <div>
+                          <Link href="/createGroup" className={styles.navLink}>Create Group    </Link> 
+                        </div>
+                        <div>
+                          <Link href="/joinGroup" className={styles.navLink}>Join Group    </Link>
+                        </div>
+                        <div>
+                          <a onClick={logout} style={{ cursor: "pointer" }} className={styles.navLink}>Logout</a> 
+                        </div>
+                      </div>  
+
+                    </>
+                    ) : (
+                      <div> 
+                      </div>
+                    )}
+
+                    {props.menu === "group" ? (
+                      <>
+                        <div className={styles.navContainer}>
+                          <div>
+                            <Link href="/dashboard" className={styles.navLink}>Dashboard </Link>
+                          </div>
+                          <div>
+                            <Link href={'/group/' + props.groupId} className={styles.navLink}>Home Page   </Link> 
+                          </div>
+                          <div>
+                            <Link href={`/membersList/${props.groupId}`} className={styles.navLink}>Members List    </Link>
+                          </div>
+                          <div>
+                            <a onClick={logout} style={{ cursor: "pointer" }} className={styles.navLink}>Logout</a> 
+                          </div>
+                        </div> 
+                      </>
+                    ) : (
+                      <div>  
+                      </div>
+                    )}
+
+
+                </div>
             </div>
           </>
         ) : ( 
